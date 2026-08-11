bf940131c4a32bf4cad6f403d4f2b6ffb87025fb
smytts <123690681+smytts@users.noreply.github.com>
2026-08-11 11:41:59 -0400

    Add: can-place-flowers - submission-13

---
+++ Data Structures & Algorithms/can-place-flowers/submission-13.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int empty = flowerbed[0] == 0 ? 1 : 0;
+
+        for (int flower : flowerbed) {
+            if (flower == 1) {
+                n -= (empty - 1) / 2;
+                empty = 0;
+            } else {
+                empty++;
+            }
+        }
+        n -= empty / 2;
+        return n <= 0;
+    }
+}
