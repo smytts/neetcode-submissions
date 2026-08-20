2e1294fc3700490d2941d434c4011aa9e4cc9146
smytts <123690681+smytts@users.noreply.github.com>
2026-08-20 13:07:07 -0400

    Add: next-greater-element-i - submission-3

---
+++ Data Structures & Algorithms/next-greater-element-i/submission-3.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> nextGreater = new HashMap<>();
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int num : nums2) {
+            while (!stack.isEmpty() && stack.peek() < num) {
+                nextGreater.put(stack.pop(), num);
+            }
+            stack.push(num);
+        }
+
+        int[] ans = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            ans[i] = nextGreater.getOrDefault(nums1[i], -1);
+        }
+        return ans;
+    }
+}
